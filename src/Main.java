3bc3ff5a51e5ac6f14043c45678eb71366686466
dreamer572 <vkirill2012@mail.ru>
2023-06-15 10:16:15 +0300

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        String output = calc(input);
+        System.out.println(output);
+    }
+    public static String calc(String input) {
+        return input;
     }
 }
