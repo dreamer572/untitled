439dbb678f74507b79b9207039dfce151707be3e
dreamer572 <vkirill2012@mail.ru>
2023-06-15 18:13:29 +0300

    Init

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,32 @@ public class Main {
         System.out.println(output);
     }
 
-
+    public static boolean isRoman(String n) {
+        return n.indexOf('I') > 0 | n.indexOf('V') > 0 | n.indexOf('X') > 0;
+    }
     public static String calc(String input) throws Exception {
+        String er = "throws Exception //";
+        //если больше двух пробелов ошибка
+        //если больше двух операндов ошибка
+        char[] ops = {'+','-','*','/'};
+        int amountOps = 0, amountSpaces = 0;
+        for (int i = 0; i < input.length(); i++)
+            if (input.charAt(i) == ' ')
+                amountSpaces++;
+            else
+                for (int j = 0; j < ops.length; j++)
+                    if (input.charAt(i) == ops[j])
+                        amountOps++;
+        if (amountOps != 1 | amountSpaces != 2)
+            throw new Exception(er+"т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+
+
+
+
+        String n1 = input.substring(0,input.indexOf(' ')), n2;
+        char op = input.charAt(input.indexOf(' ')+1);
+
+
 
         if (input.charAt(0) == 'g') throw new Exception("ggg");
 
